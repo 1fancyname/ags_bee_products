dc1a51b4bf70cbe5bb4c7f3c2dc02643634207c4
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-21 15:30:05 +0100

    Create 00_main.r

---
+++ 00_main.r
@@ -0,0 +1,18 @@
+#00_main.r
+
+
+#Run the code below to import the data, transform it and enter the command line user interface for creating 
+#the different graphs.
+source("01_load_packages.r")
+#
+source("02_import_data.r")
+#
+source("03_global_variables.r")
+#
+source("04_functions.r")
+#
+source("05_transform.r")
+#
+source("06_user_interface.r")
+
+
