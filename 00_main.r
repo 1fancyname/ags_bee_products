92667cd06b889e3019f69367fbc4cbfc75dc60a2
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-29 14:39:49 +0200

    Update 00_main.r

--- 00_main.r
+++ 00_main.r
@@ -1,22 +1,21 @@
 #00_main.r
 
 
-#
-source("01_load_packages.r")
-#
-source("02_import_ap22-25.r")
-#
-source("03_import_wachsmonitoring.r")
-#
-source("04_global_variables.r")
-#
-source("05_functions.r")
-#
-source("06_transform_ap22-25.r")
-#
-source("07_transform_wachsmonitoring.r")
-#
-source("08_user_interface.r")
+
+#To import and transform only the wax monitoring data, execute this line of code.
+source("./99_modules/run_wm.r")
+
+
+#To import and transform only the project ap22 - 25 data, execute this line of code.
+source("./99_modules/run_ap22-25.r")
+
+
+#To import and transform everything, execute this line of code.
+source("./99_modules/run_everything.r")
+
+
+#To enter the user interface for creating charts and exporting excel files, execute this line of code.
+source("./99_modules/08_user_interface.r")
 
 
 
