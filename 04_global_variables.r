98f69f07357260e24a285b46f2751170d6841138
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-29 10:22:47 +0200

    Create 04_global_variables.r

---
+++ 04_global_variables.r
@@ -0,0 +1,23 @@
+#04_global_variables.r
+
+
+#stop user interface in case it is running
+myvar.user_interface_state <- "off"
+
+
+
+
+myvar.prct_type_colours_viridis <- c("prct_gt_lod_lt_loq"="#fde725", "prct_gt_loq" = "#26828e")
+
+myvar.viridis_palette <- c("#fde725", "#b5de2b", "#6ece58", "#35b779", "#1f9e89", "#26828e", "#31688e", "#3e4989", "#482878", "#440154")
+
+
+#for user interface
+myvar.usr_matrix <- c("a")
+myvar.usr_substance <- c("")
+
+
+
+
+
+
