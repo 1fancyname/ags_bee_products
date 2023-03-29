38b7de4f2691548d3e00f3f9b352d8cadb54034b
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-29 14:39:27 +0200

    Create run_wm.r

---
+++ run_wm.r
@@ -0,0 +1,11 @@
+#run_wm.r
+
+source("01_load_packages.r")
+#
+source("03_import_wachsmonitoring.r")
+#
+source("04_global_variables.r")
+#
+source("05_functions.r")
+#
+source("07_transform_wachsmonitoring.r")
