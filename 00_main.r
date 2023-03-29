b9039515dc524ddc6e89ea3e57b6703a25be9755
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-29 10:25:36 +0200

    Update 00_main.r

--- 00_main.r
+++ 00_main.r
@@ -1,18 +1,22 @@
 #00_main.r
 
 
-#Run the code below to import the data, transform it and enter the command line user interface for creating 
-#the different graphs.
+#
 source("01_load_packages.r")
 #
-source("02_import_data.r")
+source("02_import_ap22-25.r")
 #
-source("03_global_variables.r")
+source("03_import_wachsmonitoring.r")
 #
-source("04_functions.r")
+source("04_global_variables.r")
 #
-source("05_transform.r")
+source("05_functions.r")
 #
-source("06_user_interface.r")
+source("06_transform_ap22-25.r")
+#
+source("07_transform_wachsmonitoring.r")
+#
+source("08_user_interface.r")
+
 
 
