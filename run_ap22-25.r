b3af89e1020ac7509cbe5e463e633aea9dffc6b4
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-03-29 14:39:10 +0200

    Create run_ap22-25.r

---
+++ run_ap22-25.r
@@ -0,0 +1,11 @@
+#run_ap22-25.r
+
+source("./99_modules/01_load_packages.r")
+#
+source("./99_modules/02_import_ap22-25.r")
+#
+source("./99_modules/04_global_variables.r")
+#
+source("./99_modules/05_functions.r")
+#
+source("./99_modules/06_transform_ap22-25.r")
