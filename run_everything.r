c0957083b8a0b8e08240ad7b9a3b471703a6f52d
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-04-06 14:33:46 +0200

    Update run_everything.r

--- run_everything.r
+++ run_everything.r
@@ -1,18 +1,18 @@
 #run_everything.r
 
 
-source("01_load_packages.r")
+source("./99_modules/01_load_packages.r")
 #
-source("02_import_ap22-25.r")
+source("./99_modules/02_import_ap22-25.r")
 #
-source("03_import_wachsmonitoring.r")
+source("./99_modules/03_import_wachsmonitoring.r")
 #
-source("04_global_variables.r")
+source("./99_modules/04_global_variables.r")
 #
-source("05_functions.r")
+source("./99_modules/05_functions.r")
 #
-source("06_transform_ap22-25.r")
+source("./99_modules/06_transform_ap22-25.r")
 #
-source("07_transform_wachsmonitoring.r")
+source("./99_modules/07_transform_wachsmonitoring.r")
 #
-source("08_user_interface.r")
+source("./99_modules/08_user_interface.r")
