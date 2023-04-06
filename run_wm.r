1e8ea1106b6b00b630196f44e7011c3d30e24eb4
1fancyname <106318340+1fancyname@users.noreply.github.com>
2023-04-06 14:34:03 +0200

    Update run_wm.r

--- run_wm.r
+++ run_wm.r
@@ -1,11 +1,11 @@
 #run_wm.r
 
-source("01_load_packages.r")
+source("./99_modules/01_load_packages.r")
 #
-source("03_import_wachsmonitoring.r")
+source("./99_modules/03_import_wachsmonitoring.r")
 #
-source("04_global_variables.r")
+source("./99_modules/04_global_variables.r")
 #
-source("05_functions.r")
+source("./99_modules/05_functions.r")
 #
-source("07_transform_wachsmonitoring.r")
+source("./99_modules/07_transform_wachsmonitoring.r")
