e1f473b2b6ad6a475ba103c3f3e2f3ecb05fb90e
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-04-23 17:14:52 +0200

    Create 10_states.r

---
+++ 10_states.r
@@ -0,0 +1,14 @@
+#10_states.r
+
+#stop user interface in case it is running
+myvar.user_interface_state <- "off"
+
+myvar.packages_loaded <- FALSE
+myvar.gvar_loaded <- FALSE
+myvar.fun_loaded <- FALSE
+myvar.pm_ppp_loaded <- FALSE
+myvar.pm_ppp_processed <- FALSE
+myvar.wm_loaded <- FALSE
+myvar.wm_processed <-  FALSE
+myvar.ap22_25_loaded <- FALSE
+myvar.ap22_25_processed <- FALSE
