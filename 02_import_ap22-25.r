4916dfe3fefdf978c5db3ad4b8559407233c3298
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-04-24 17:12:56 +0200

    Update 02_import_ap22-25.r

--- 02_import_ap22-25.r
+++ 02_import_ap22-25.r
@@ -1,7 +1,8 @@
 #02_import_ap22-25.r
 
-#stop user interface in case it is running
-myvar.user_interface_state <- "off"
+
+base::print("% Getting working directory")
+myvar.cur_wd <- as.character(getwd())
 
 
 base::print("% Importing project data.")
@@ -13,62 +14,66 @@ script_parameters <- readxl::read_excel("script_parameters.xlsx")
 
 
 #get user input from excel by filtering on variable names and subsetting
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_doc")
-myvar.sample_doc <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_project_data")
+myvar.ap22_25_project_data <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_bb_sheet")
-myvar.samples_bb_sheet <- tmp_tbl_params$user_input[1]
+#get user input from excel by filtering on variable names and subsetting
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_doc")
+myvar.ap22_25_samples_doc <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_p_sheet")
-myvar.samples_p_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_bb_sheet")
+myvar.ap22_25_samples_bb_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_w_sheet")
-myvar.samples_w_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_p_sheet")
+myvar.ap22_25_samples_p_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_a_sp_sheet")
-myvar.samples_a_sp_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_w_sheet")
+myvar.ap22_25_samples_w_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.samples_a_sheet")
-myvar.samples_a_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_a_sp_sheet")
+myvar.ap22_25_samples_a_sp_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_doc")
-myvar.results_doc <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_samples_a_sheet")
+myvar.ap22_25_samples_a_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_bb_sheet")
-myvar.results_bb_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_doc")
+myvar.ap22_25_results_doc <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_p_sheet")
-myvar.results_p_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_bb_sheet")
+myvar.ap22_25_results_bb_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_w_sheet")
-myvar.results_w_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_p_sheet")
+myvar.ap22_25_results_p_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_a_sheet")
-myvar.results_a_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_w_sheet")
+myvar.ap22_25_results_w_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.results_a_sp_sheet")
-myvar.results_a_sp_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_a_sheet")
+myvar.ap22_25_results_a_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_doc")
-myvar.meta_doc <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_results_a_sp_sheet")
+myvar.ap22_25_results_a_sp_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_loc_sheet")
-myvar.meta_loc_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_doc")
+myvar.ap22_25_meta_doc <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_col_sheet")
-myvar.meta_col_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_loc_sheet")
+myvar.ap22_25_meta_loc_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_exp_sheet")
-myvar.meta_exp_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_col_sheet")
+myvar.ap22_25_meta_col_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_lim_sheet")
-myvar.meta_lim_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_exp_sheet")
+myvar.ap22_25_meta_exp_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_mat_sheet")
-myvar.meta_mat_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_lim_sheet")
+myvar.ap22_25_meta_lim_sheet <- tmp_tbl_params$user_input[1]
 
-tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.meta_sub_sheet")
-myvar.meta_sub_sheet <- tmp_tbl_params$user_input[1]
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_mat_sheet")
+myvar.ap22_25_meta_mat_sheet <- tmp_tbl_params$user_input[1]
+
+tmp_tbl_params <- dplyr::filter(script_parameters, parameter_name == "myvar.ap22_25_meta_sub_sheet")
+myvar.ap22_25_meta_sub_sheet <- tmp_tbl_params$user_input[1]
 
 rm(tmp_tbl_params)
 
@@ -80,38 +85,39 @@ rm(tmp_tbl_params)
 
 
 #metadata tables
-tbl_colonies <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_col_sheet)
-tbl_locations <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_loc_sheet)
-tbl_experiments <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_exp_sheet)
-tbl_limits <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_lim_sheet)
-tbl_matrixes <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_mat_sheet)
-tbl_substances <- readxl::read_excel(myvar.meta_doc, sheet = myvar.meta_sub_sheet)
+tbl_colonies <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_col_sheet)
+tbl_locations <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_loc_sheet)
+tbl_experiments <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_exp_sheet)
+tbl_limits <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_lim_sheet)
+tbl_matrixes <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_mat_sheet)
+tbl_substances <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_meta_doc), sheet = myvar.ap22_25_meta_sub_sheet)
 
 
 
 #result tables
-tbl_results_bb <- readxl::read_excel(myvar.results_doc, sheet = myvar.results_bb_sheet)
-tbl_results_p <- readxl::read_excel(myvar.results_doc, sheet = myvar.results_p_sheet)
-tbl_results_w <- readxl::read_excel(myvar.results_doc, sheet = myvar.results_w_sheet)
-tbl_results_a <- readxl::read_excel(myvar.results_doc, sheet = myvar.results_a_sheet)
-tbl_results_a_sp <- readxl::read_excel(myvar.results_doc, sheet = myvar.results_a_sp_sheet)
+tbl_results_bb <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_results_doc), sheet = myvar.ap22_25_results_bb_sheet)
+tbl_results_p <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_results_doc), sheet = myvar.ap22_25_results_p_sheet)
+tbl_results_w <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_results_doc), sheet = myvar.ap22_25_results_w_sheet)
+tbl_results_a <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_results_doc), sheet = myvar.ap22_25_results_a_sheet)
+tbl_results_a_sp <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_results_doc), sheet = myvar.ap22_25_results_a_sp_sheet)
 
 
 
 #sample tables
-tbl_samples_bb <- readxl::read_excel(myvar.sample_doc, sheet = myvar.samples_bb_sheet)
-tbl_samples_p <- readxl::read_excel(myvar.sample_doc, sheet = myvar.samples_p_sheet)
-tbl_samples_w <- readxl::read_excel(myvar.sample_doc, sheet = myvar.samples_w_sheet)
-tbl_samples_a <- readxl::read_excel(myvar.sample_doc, sheet = myvar.samples_a_sheet)
-tbl_samples_a_sp <- readxl::read_excel(myvar.sample_doc, sheet = myvar.samples_a_sp_sheet)
+tbl_samples_bb <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_samples_doc), sheet = myvar.ap22_25_samples_bb_sheet)
+tbl_samples_p <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_samples_doc), sheet = myvar.ap22_25_samples_p_sheet)
+tbl_samples_w <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_samples_doc), sheet = myvar.ap22_25_samples_w_sheet)
+tbl_samples_a <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_samples_doc), sheet = myvar.ap22_25_samples_a_sheet)
+tbl_samples_a_sp <- readxl::read_excel(paste0(myvar.cur_wd, myvar.ap22_25_project_data, myvar.ap22_25_samples_doc), sheet = myvar.ap22_25_samples_a_sp_sheet)
 
 
-rm(myvar.meta_col_sheet, myvar.meta_doc, myvar.meta_exp_sheet,
-   myvar.meta_lim_sheet, myvar.meta_loc_sheet, myvar.meta_mat_sheet,
-   myvar.meta_sub_sheet, myvar.results_bb_sheet, myvar.results_doc,
-   myvar.results_p_sheet, myvar.results_w_sheet, myvar.results_a_sheet, myvar.sample_doc,
-   myvar.samples_bb_sheet, myvar.samples_p_sheet, myvar.samples_w_sheet, myvar.samples_a_sheet, 
-   myvar.results_a_sp_sheet, myvar.samples_a_sp_sheet)
+rm(myvar.ap22_25_project_data, myvar.ap22_25_samples_doc, myvar.ap22_25_samples_bb_sheet,
+   myvar.ap22_25_samples_p_sheet, myvar.ap22_25_samples_w_sheet, myvar.ap22_25_samples_a_sp_sheet,
+   myvar.ap22_25_samples_a_sheet, myvar.ap22_25_results_doc, myvar.ap22_25_results_bb_sheet,
+   myvar.ap22_25_results_p_sheet, myvar.ap22_25_results_w_sheet, myvar.ap22_25_results_a_sheet,
+   myvar.ap22_25_results_a_sp_sheet, myvar.ap22_25_meta_doc, myvar.ap22_25_meta_loc_sheet, 
+   myvar.ap22_25_meta_col_sheet, myvar.ap22_25_meta_exp_sheet, myvar.ap22_25_meta_lim_sheet,
+   myvar.ap22_25_meta_mat_sheet, myvar.ap22_25_meta_sub_sheet, myvar.cur_wd)
 
 
 
