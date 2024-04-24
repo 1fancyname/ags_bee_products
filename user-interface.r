261701dc1294a4aa75877ee03387b8c39b8254a0
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-04-24 17:19:49 +0200

    Update user-interface.r

--- user-interface.r
+++ user-interface.r
@@ -13,31 +13,33 @@ while (myvar.user_interface_state != "off") {
   base::print("")
   base::print("")
   base::print("Current status:")
-  base::print(paste0("Wachsmonitoring data is loaded:         ", myvar.wm_loaded))
-  base::print(paste0("Wachsmonitoring data is processed:      ", myvar.wm_processed))
+  base::print(paste0("PPP Wachsmonitoring data is loaded:         ", myvar.wm_loaded))
+  base::print(paste0("PPP Wachsmonitoring data is processed:      ", myvar.wm_processed))
   base::print(paste0("Project AP22-25 data is loaded:         ", myvar.ap22_25_loaded))
   base::print(paste0("Project AP22-25 data is processed:      ", myvar.ap22_25_processed))
   base::print(paste0("PPP Pollenmonitoring data is loaded:    ", myvar.pm_ppp_loaded))
   base::print(paste0("PPP Pollenmonitoring data is processed: ", myvar.pm_ppp_processed))
   base::print("What project would you like to work on?")
   base::print("Type ")
-  base::print("1 to work with data from Project Wachsmonitoring.")
+  base::print("1 to work with data from Project PPP Wachsmonitoring.")
   base::print("2 to work with data from Project AP22-25")
   base::print("3 to work with data from Project PPP Pollenmonitoring")
   myvar.usr.choose_project <- base::as.character(base::readline("Enter here:")) 
   switch(myvar.usr.choose_project,
          "1" = {
-           base::print("Loading Wachsmonitoring data... ")
-           
-           
+           base::print("Loading PPP Wachsmonitoring data... ")
+           source("./99_modules/load_wachsmonitoring_ppp.r")
+           source("./99_modules/14_user_interface_wm_ppp.r")
          },
          "2" = {
-           #
+           base::print("% Loading Project AP22-25 data... ")
+           source("./99_modules/load_ap22-25.r")
+           source("./99_modules/08_user_interface_ap22-25.r")
          },
          "3" = {
-           base::print("Loading PPP Pollenmonitoring data... ")
+           base::print("% Loading PPP Pollenmonitoring data... ")
            source("./99_modules/load_pollenmonitoring_ppp.r")
-           source("./99_modules/11_user-interface_pm_ppp.r")
+           source("./99_modules/11_user-interface_pollenmonitoring_ppp.r")
          },
          "exit" = {
            base::print("% Stopping user interface...")
