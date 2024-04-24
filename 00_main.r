f313a5d1d5beafabb82ec21b602659028788ce3a
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-04-24 17:12:22 +0200

    Update 00_main.r

--- 00_main.r
+++ 00_main.r
@@ -6,30 +6,3 @@ source("./99_modules/user-interface.r")
 
 
 
-
-
-
-
-
-
-# Archive -----------------------------------------------------------------
-
-
-
-#To import and transform only the wax monitoring data, execute this line of code.
-source("./99_modules/run_wm.r")
-
-
-#To import and transform only the project ap22 - 25 data, execute this line of code.
-source("./99_modules/run_ap22-25.r")
-
-
-#To import and transform everything, execute this line of code.
-source("./99_modules/run_everything.r")
-
-
-#To enter the user interface for creating charts and exporting excel files, execute this line of code.
-source("./99_modules/08_user_interface_ap22-25.r")
-
-
-
