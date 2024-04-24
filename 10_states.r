b8a0b7a7d43fcd42aa209945a9926ffc3632053e
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-04-24 17:15:46 +0200

    Update 10_states.r

--- 10_states.r
+++ 10_states.r
@@ -7,8 +7,11 @@ myvar.packages_loaded <- FALSE
 myvar.gvar_loaded <- FALSE
 myvar.fun_loaded <- FALSE
 myvar.pm_ppp_loaded <- FALSE
+myvar.pm_ppp_re_load <- FALSE
 myvar.pm_ppp_processed <- FALSE
 myvar.wm_loaded <- FALSE
+myvar.wm_re_load <- FALSE
 myvar.wm_processed <-  FALSE
 myvar.ap22_25_loaded <- FALSE
+myvar.ap22_25_re_load <- FALSE
 myvar.ap22_25_processed <- FALSE
