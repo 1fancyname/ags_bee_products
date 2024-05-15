6eed8116867fdba40ee3388218eb380c7608be61
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-05-15 16:29:57 +0200

    Update 07_transform_wachsmonitoring_ppp.r

--- 07_transform_wachsmonitoring_ppp.r
+++ 07_transform_wachsmonitoring_ppp.r
@@ -1,11 +1,17 @@
 #07_transform_wachsmonitoring_ppp.r
 
 
-base::print("% Transforming wachsmonitoring results table to long format.")
 
-tbl_results_wm <- tbl_results_wm %>% 
+
+
+# tbl_wm_ppp_pool ---------------------------------------------------------
+
+
+base::print("% Transforming PPP wachsmonitoring pool results table to long format.")
+
+tbl_wm_ppp_pool <- tbl_wm_ppp_pool %>% 
   tidyr::pivot_longer(
-    cols = "Acetamiprid":"Thymol", 
+    cols = "Fludioxonil":"DEET", 
     names_to = "substance", 
     values_to = "concentration",
     values_drop_na = FALSE
@@ -14,7 +20,34 @@ tbl_results_wm <- tbl_results_wm %>%
 
 
 #replace all NA values with 0
-tbl_results_wm$concentration <- base::replace(tbl_results_wm$concentration, base::is.na(tbl_results_wm$concentration), 0)
+tbl_wm_ppp_pool$concentration <- base::replace(tbl_wm_ppp_pool$concentration, base::is.na(tbl_wm_ppp_pool$concentration), 0)
+
+
+
+
+
+# tbl_wm_ppp_yearly -------------------------------------------------------
+
+base::print("% Transforming PPP wachsmonitoring yearly results table to long format.")
+
+tbl_wm_ppp_yearly <- tbl_wm_ppp_yearly %>% 
+  tidyr::pivot_longer(
+    cols = "Fludioxonil":"Thymol", 
+    names_to = "substance", 
+    values_to = "concentration",
+    values_drop_na = FALSE
+  )
+
+
+
+#replace all NA values with 0
+tbl_wm_ppp_yearly$concentration <- base::replace(tbl_wm_ppp_yearly$concentration, base::is.na(tbl_wm_ppp_yearly$concentration), 0)
+
+
+
+
+
+
 
 myvar.wm_processed <- TRUE
 base::print("% processing of PPP Wachsmonitoring data successfull.")
