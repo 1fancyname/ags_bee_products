d8ae08e78675396018d71ed7e981ef69a759d375
1fancyname <106318340+1fancyname@users.noreply.github.com>
2024-05-21 11:02:18 +0200

    Update 03_import_wachsmonitoring_ppp.r

--- 03_import_wachsmonitoring_ppp.r
+++ 03_import_wachsmonitoring_ppp.r
@@ -4,8 +4,6 @@ base::print("% Getting working directory")
 myvar.cur_wd <- as.character(getwd())
 
 
-
-base::print("% Getting working directory")
 myvar.cur_wd <- as.character(getwd())
 
 base::print("% Importing ppp wachsmonitoring project data.")
@@ -36,6 +34,5 @@ myvar.wm_ppp_results_yearly_sheet <- tmp_tbl_params$user_input[1]
 # import excel files ------------------------------------------------------
 
 
-#metadata tables
 tbl_wm_ppp_pool <- readxl::read_excel(paste0(myvar.cur_wd, myvar.wm_ppp_project_data, myvar.wm_ppp_results_doc), sheet = myvar.wm_ppp_results_pool_sheet)
 tbl_wm_ppp_yearly <- readxl::read_excel(paste0(myvar.cur_wd, myvar.wm_ppp_project_data, myvar.wm_ppp_results_doc), sheet = myvar.wm_ppp_results_yearly_sheet)
